854f751850eda7ccc13d2312f81a0b14d9af15e7
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-05-19 00:08:06 -0700

    Add xp calculator

---
+++ core/xp-calculator.ts
@@ -0,0 +1,27 @@
+function xpNeededToReachNextLevel(currentLevel: number): number {
+    if (currentLevel >= 0 && currentLevel <= 15)
+        return 2 * currentLevel + 7;
+
+    if (currentLevel >= 16 && currentLevel <= 30)
+        return 5 * currentLevel - 38;
+
+    if (currentLevel >= 31)
+        return 9 * currentLevel - 158;
+
+    console.warn("Unknown level: " + currentLevel);
+}
+
+function xpNeededToReachLevel(currentLevel: number, targetLevel: number): number {
+    let xpNeeded: number = 0;
+
+    for (let i: number = currentLevel; i < targetLevel; i++) {
+        xpNeeded += xpNeededToReachNextLevel(i);
+    }
+
+    return xpNeeded;
+}
+
+export default {
+    xpNeededToReachLevel,
+    xpNeededToReachNextLevel,
+};
