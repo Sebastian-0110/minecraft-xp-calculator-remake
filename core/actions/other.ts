e9b20a49a0c688a00eabb34997c5ffb4c0ad0940
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-05-18 22:34:22 -0700

    Add other actions

---
+++ core/actions/other.ts
@@ -0,0 +1,23 @@
+import { Action } from "./types/action";
+
+export const other: Action[] = [
+    {
+        sources: ["Bottle O' Enchanting"],
+        xpPerAction: { min: 3, max: 11 },
+    },
+
+    {
+        sources: ["Trading With Villager"],
+        xpPerAction: { min: 3, max: 6 },
+    },
+
+    {
+        sources: ["Breding Animals"],
+        xpPerAction: { min: 1, max: 7 },
+    },
+
+    {
+        sources: ["Catching Fish"],
+        xpPerAction: { min: 1, max: 6 },
+    },
+];
