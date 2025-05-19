12397bf25a38fda4e84ce05af854934a59f31768
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-05-18 22:31:06 -0700

    Add bosses actions

---
+++ core/actions/bosses.ts
@@ -0,0 +1,13 @@
+import { Action } from "./types/action";
+
+export const bosses: Action[] = [
+    {
+        sources: ["Enderdragon"],
+        xpPerAction: { value: 12000 },
+    },
+
+    {
+        sources: ["Wither"],
+        xpPerAction: { value: 50 },
+    },
+];
