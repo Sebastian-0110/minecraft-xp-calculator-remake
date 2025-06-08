b09479045354a6c7eaee5a41f3489fc23f2db0bf
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-06-07 23:53:22 -0700

    Add export entrypoint to actions module

---
+++ core/src/actions/index.ts
@@ -0,0 +1,7 @@
+import { bosses } from "./bosses";
+import { mining } from "./mining";
+import { mobs } from "./mobs";
+import { other } from "./other";
+import { smeltingAndCooking } from "./smelting-and-cooking";
+
+export { bosses, mining, mobs, other, smeltingAndCooking };
