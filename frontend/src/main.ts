155e3d3a285c93eac0cfd07374bdece84bf83442
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-06-08 13:35:56 -0700

    Install primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import Lara from "@primeuix/themes/lara";
+import Button from "primevue/button";
+
+import './style.css';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+    },
+});
+
+app.component("Button", Button);
+
+app.mount('#app');
